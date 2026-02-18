47d53343da975778268ee454cce7fa4db2d667dc
rodrigosuza <rodrigomax620@gmail.com>
2026-02-18 17:44:37 -0300

    feat: add `App.jsx` to render an invitation with interactive confirmation and location buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import invitationImg from './assets/iara.jpg'
 
 function App() {
   const whatsappLink = "https://wa.me/5594991016930?text=Ol%C3%A1%20noivos%2C%20eu%20estou%20confirmando%20minha%20presen%C3%A7a!!";
+  const mapsLink = "https://www.google.com/maps/place/8%C2%B002'19.3%22S+50%C2%B000'01.0%22W/@-8.0386829,-50.0028534,17z/data=!3m1!4b1!4m4!3m3!8m2!3d-8.0386829!4d-50.0002785?hl=pt-BR&entry=ttu&g_ep=EgoyMDI2MDIxNi4wIKXMDSoASAFQAw%3D%3D";
 
   const handleConfirmation = () => {
     window.location.href = whatsappLink;
+  };
+
+  const handleLocation = () => {
+    window.open(mapsLink, '_blank');
   };
 
   return (
@@ -17,7 +22,7 @@ function App() {
           loading="eager"
         />
 
-        {/* Botão Invisível apenas sobre 'Confirmação de Presença' */}
+        {/* Botão Invisível: Confirmação de Presença */}
         <button
           onClick={handleConfirmation}
           className="absolute bg-transparent border-none cursor-pointer outline-none focus:outline-none"
@@ -29,6 +34,20 @@ function App() {
             zIndex: 10
           }}
           aria-label="Confirmar Presença"
+        />
+
+        {/* Botão Invisível: Localização */}
+        <button
+          onClick={handleLocation}
+          className="absolute bg-transparent border-none cursor-pointer outline-none focus:outline-none"
+          style={{
+            top: '57%',
+            left: '37%',
+            width: '26%',
+            height: '14%',
+            zIndex: 10
+          }}
+          aria-label="Ver Localização"
         />
       </div>
     </div>
