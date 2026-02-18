d0cf428a7af8fe17a800b81e3df397a9326023c5
rodrigosuza <rodrigomax620@gmail.com>
2026-02-18 17:49:15 -0300

    UX: Adicionado botão da Lista de Presentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import invitationImg from './assets/iara.jpg'
 function App() {
   const whatsappLink = "https://wa.me/5594991016930?text=Ol%C3%A1%20noivos%2C%20eu%20estou%20confirmando%20minha%20presen%C3%A7a!!";
   const mapsLink = "https://www.google.com/maps/place/8%C2%B002'19.3%22S+50%C2%B000'01.0%22W/@-8.0386829,-50.0028534,17z/data=!3m1!4b1!4m4!3m3!8m2!3d-8.0386829!4d-50.0002785?hl=pt-BR&entry=ttu&g_ep=EgoyMDI2MDIxNi4wIKXMDSoASAFQAw%3D%3D";
+  const listsLink = "https://ronald-iara.vercel.app/";
 
   const handleConfirmation = () => {
     window.location.href = whatsappLink;
@@ -10,6 +11,10 @@ function App() {
 
   const handleLocation = () => {
     window.open(mapsLink, '_blank');
+  };
+
+  const handleGifts = () => {
+    window.open(listsLink, '_blank');
   };
 
   return (
@@ -48,6 +53,20 @@ function App() {
             zIndex: 10
           }}
           aria-label="Ver Localização"
+        />
+
+        {/* Botão Invisível: Lista de Presentes */}
+        <button
+          onClick={handleGifts}
+          className="absolute bg-transparent border-none cursor-pointer outline-none focus:outline-none"
+          style={{
+            top: '57%',
+            left: '64%',
+            width: '26%',
+            height: '14%',
+            zIndex: 10
+          }}
+          aria-label="Lista de Presentes"
         />
       </div>
     </div>
